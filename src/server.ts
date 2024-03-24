8aae15d17ffc59a638118d22f9cfc368197641ed
AbhijitKr <abhijitkr600@gmail.com>
2024-03-24 19:22:12 +0530

    Connects database

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
+import "dotenv/config";
 import express from "express";
+import { connectDb } from "./config/db.config";
 const app = express();
-const PORT = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+// app.post('/', )
+connectDb();
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
